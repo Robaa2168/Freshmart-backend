19f05793e080b454c3ed23c2d358c17758cc9e9a
ROBERT <robertlagat38@gmail.com>
2023-11-24 16:15:08 +0300

    first commit

--- config/auth.js
+++ config/auth.js
@@ -34,7 +34,6 @@ const tokenForVerify = (user) => {
 
 const isAuth = async (req, res, next) => {
   const { authorization } = req.headers;
-  // console.log('authorization',authorization)
   try {
     const token = authorization.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
